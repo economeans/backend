28bc2b46fda72058b3de793394be614b84ee6a21
hoeseong <hoeseong19@naver.com>
2024-07-02 19:32:48 +0900

    initial commit

---
+++ src/test/java/com/gapjincoup/economeans/EconomeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapjincoup.economeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
