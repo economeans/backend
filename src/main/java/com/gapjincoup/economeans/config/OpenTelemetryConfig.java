cea7e11300ec532485f02d750094d0087343aeae
hoeseong <hoeseong19@naver.com>
2024-07-09 01:23:44 +0900

    Chore: OpenTelemetryConfig에 Tracer 추가

--- src/main/java/com/gapjincoup/economeans/config/OpenTelemetryConfig.java
+++ src/main/java/com/gapjincoup/economeans/config/OpenTelemetryConfig.java
@@ -1,6 +1,7 @@
 package com.gapjincoup.economeans.config;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class OpenTelemetryConfig {
     @Bean
     public OpenTelemetry openTelemetry() {
         return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+    }
+
+    @Bean
+    public Tracer tracer(OpenTelemetry openTelemetry) {
+        return openTelemetry.getTracer("Economeans");
     }
 }
