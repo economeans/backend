f68adea8cee6dc6c8984b7d783d4814d14584c06
hoeseong <hoeseong19@naver.com>
2024-07-12 03:24:21 +0900

    Feat: csrf disable

--- src/main/java/com/gapjincoup/economeans/security/WebSecurityConfig.java
+++ src/main/java/com/gapjincoup/economeans/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,16 +15,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeHttpRequests -> {
-                    authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/**").permitAll();
-                    authorizeHttpRequests.requestMatchers(HttpMethod.GET, "/**").permitAll();
-                    authorizeHttpRequests.requestMatchers(HttpMethod.PUT, "/**").permitAll();
-                    authorizeHttpRequests.requestMatchers(HttpMethod.PATCH, "/**").permitAll();
-                    authorizeHttpRequests.requestMatchers(HttpMethod.DELETE, "/**").permitAll();
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorizeHttpRequests -> {
+            authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/**").permitAll();
+            authorizeHttpRequests.requestMatchers(HttpMethod.GET, "/**").permitAll();
+            authorizeHttpRequests.requestMatchers(HttpMethod.PUT, "/**").permitAll();
+            authorizeHttpRequests.requestMatchers(HttpMethod.PATCH, "/**").permitAll();
+            authorizeHttpRequests.requestMatchers(HttpMethod.DELETE, "/**").permitAll();
 
-                    authorizeHttpRequests.anyRequest().authenticated();
-                });
+            authorizeHttpRequests.anyRequest().authenticated();
+        });
 
         return http.build();
     }
