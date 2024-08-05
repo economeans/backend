697e4b44fa2d7d593407668a5d77fb5e12fcddd0
inh3011 <inh3011@gmail.com>
2024-08-05 13:16:32 +0900

    #23 feat: Uri 생성 테스트

--- src/test/java/com/gapjincoup/economeans/infrastructure/EcosClientTest.java
+++ src/test/java/com/gapjincoup/economeans/infrastructure/EcosClientTest.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class EcosClientTest {
@@ -28,24 +26,24 @@ class EcosClientTest {
 
         String baseUrl = "https://ecos.bok.or.kr/api/StatisticWord/FFMI99QPYH9TBZQU6JS2/json/kr/1/10/소비자동향지수";
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl);
+        String uriString = UriComponentsBuilder.fromHttpUrl(baseUrl).encode().toUriString();
 
-        ResponseEntity<Object> response = restTemplate.exchange(
-                builder.toUriString(),
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
+        ResponseEntity<Objects> forEntity = restTemplate.getForEntity(uriString, Objects.class);
+    }
 
-        ResponseEntity<Objects> terms = ecosClient.getTerms(
-                "StatisticWord",
-                "FFMI99QPYH9TBZQU6JS2",
-                "json",
-                "kr",
-                1,
-                10,
-                "소비자동향지수"
-        );
+    @Test
+    void makeBaseUri() {
+        String baseUrl = "https://ecos.bok.or.kr/api";
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .pathSegment("StatisticWord")
+                .pathSegment("FFMI99QPYH9TBZQU6JS2")
+                .pathSegment("json")
+                .pathSegment("kr")
+                .pathSegment(String.valueOf(1))
+                .pathSegment(String.valueOf(10))
+                .pathSegment("소비자동향지수");
+
+        assertEquals(builder.toUriString(), "https://ecos.bok.or.kr/api/StatisticWord/FFMI99QPYH9TBZQU6JS2/json/kr/1/10/%EC%86%8C%EB%B9%84%EC%9E%90%EB%8F%99%ED%96%A5%EC%A7%80%EC%88%98");
     }
 }
